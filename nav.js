e36f4a6c6e33fb3b41b35a75875e019fd41a29c9
Eric Beauce <ebeauce@ldeo.columbia.edu>
2023-08-05 15:35:39 -0400

    html

---
+++ nav.js
@@ -0,0 +1,10 @@
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    // newelem create a new element with division of class menu
+    let newelem = document.createElement("div");
+    newelem.setAttribute("class","menu");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
